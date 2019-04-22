7c5c46edfb402a00468863eb749b0c88bc244458
redjamhere <aynur.zaymanov@mail.ru>
2019-04-23 01:42:26 +0300

    fix_index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/components/Home'
+import Home from '@/components/home'
 import Login from '@/components/Auth/login'
 import Registration from '@/components/Auth/registration'
 import Calculate from '@/components/calculate'
